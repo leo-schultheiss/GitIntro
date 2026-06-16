c53c0a6c2f847bb5616340fb7931cbfd72887085
Leo Schultheiß <leo.schultheiss@fau.de>
2026-06-16 09:56:58 +0200

    lese Daten aus

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os, csv
+
 def main():
     print("Hello World!")
+
+    csv_path = "data.csv"
+    with open(csv_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            print(row)
 
 
 if __name__ == "__main__":
